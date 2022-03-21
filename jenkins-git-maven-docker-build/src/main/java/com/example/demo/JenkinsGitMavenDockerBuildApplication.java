8e6cd4c35e230d326bd5e38001b46ae53e167268
Gopikrishna666 <95918200+Gopikrishna666@users.noreply.github.com>
2022-03-21 17:39:43 +0530

    Update JenkinsGitMavenDockerBuildApplication.java

--- jenkins-git-maven-docker-build/src/main/java/com/example/demo/JenkinsGitMavenDockerBuildApplication.java
+++ jenkins-git-maven-docker-build/src/main/java/com/example/demo/JenkinsGitMavenDockerBuildApplication.java
@@ -8,6 +8,7 @@ public class JenkinsGitMavenDockerBuildApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsGitMavenDockerBuildApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
